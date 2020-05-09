f7cda285be6fbc23ba4bcb55e5889f1779854434
Drew Beckmen <drew.beckmen@yale.edu>
2020-05-09 20:23:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(str)
   string_array = str.split(" ")
-  reversed = string_array.collect {|word| word.reverse}
   final string = ""
+  reversed = string_array.collect {|word| word.reverse}
+  
   
 end 
